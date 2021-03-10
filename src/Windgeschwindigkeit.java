442d2fb6bfa17559d5a00af7e63bfcb639eb2de1
BarbaraSachLackner <barbara-edith.sach-lackner@fh-burgenland.at>
2021-03-10 20:34:20 +0100

    added scanner.nextLine parse double from String to avoid input mismatch

--- src/Windgeschwindigkeit.java
+++ src/Windgeschwindigkeit.java
@@ -1,6 +1,5 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -26,9 +25,11 @@ public class Windgeschwindigkeit extends WindDaten {
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println("Wie viele Geschwindigkeiten wollen Sie erfassen: ");
         int anzahl = scanner.nextInt();
+        scanner.nextLine(); //muss IMMEr nach einem nextInt() etc gemacht werden. sonst kann es zu komische nebenwirkungen kommen.
         for (int i = 0; i < anzahl; i++) {
             System.out.println("Bitte die Windgeschwindigkeit eingeben: ");
             double speed = scanner.nextDouble();
+            scanner.nextLine();
             windSpeedList.add(new Windgeschwindigkeit(i, localDateTime, speed));
         }
         for (int j = 0; j < anzahl; j++) {
@@ -39,12 +40,8 @@ public class Windgeschwindigkeit extends WindDaten {
     private double readSpeed() {
 
         while (true) {
-            try {
-                return scanner.nextDouble();
-            } catch (InputMismatchException e) {
-                System.out.println("Falsche Eingabe! Bitte Integer oder Double eingeben:");
-                scanner.nextLine();
-            }
+            String d = scanner.nextLine();
+            return Double.parseDouble(d);
         }
     }
 
